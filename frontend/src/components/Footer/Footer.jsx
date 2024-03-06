9c88513ef25e0d3186a37bfe8f7c54299f3bd728
Shrabya35 <shrabyaraj@gmail.com>
2024-03-06 21:09:11 +0545

    Footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,11 +1,133 @@
-// Footer.js
 import React from "react";
+import "./Footer.css";
+
+import Esewa from "../../assets/e_sewa.png";
+import Khalti from "../../assets/khalti.png";
+
+import {
+  FaDiscord,
+  FaFacebookF,
+  FaYoutube,
+  FaInstagram,
+  FaTiktok,
+} from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <footer className="Footer">
-      <p>&copy; 2024 Your Company</p>
-    </footer>
+    <div className="Footer">
+      <div className="footer-container footer-flex">
+        <div className="footer-content footer-flex">
+          <div className="footer-right footer-flex">
+            <div className="footer-right-item footer-flex">
+              <div className="footer-content-title">
+                <h3>LynxLine</h3>
+              </div>
+              <div className="footer-links footer-flex">
+                <ul>
+                  <li className="footer-link footer-links-lynxline">
+                    <a href="/">About LynxLine</a>
+                  </li>
+                  <li className="footer-link footer-links-lynxline">
+                    <a href="/">Careers</a>
+                  </li>
+                  <li className="footer-link footer-links-lynxline">
+                    <a href="/">Terms & Conditions</a>
+                  </li>
+                  <li className="footer-link footer-links-lynxline">
+                    <a href="/">Privacy Policy</a>
+                  </li>
+                  <li className="footer-link footer-links-lynxline">
+                    <a href="/">Return Policy</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="footer-right-item footer-flex">
+              <div className="footer-content-title">
+                <h3>Pages</h3>
+              </div>
+              <div className="footer-links footer-flex">
+                <ul>
+                  <li className="footer-link footer-links-pages">
+                    <a href="/">Mens</a>
+                  </li>
+                  <li className="footer-link footer-links-pages">
+                    <a href="/">Womens</a>
+                  </li>
+                  <li className="footer-link footer-links-pages">
+                    <a href="/">Accesories</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="footer-right-item footer-flex">
+              <div className="footer-content-title">
+                <h3>Help</h3>
+              </div>
+              <div className="footer-links footer-flex">
+                <ul>
+                  <li className="footer-link footer-links-help">
+                    <a href="/">Contact Us</a>
+                  </li>
+                  <li className="footer-link footer-links-help">
+                    <a href="/">Returns</a>
+                  </li>
+                  <li className="footer-link footer-links-help">
+                    <a href="/">Orders</a>
+                  </li>
+                  <li className="footer-link footer-links-help">
+                    <a href="/auth/reset-password">Reset Password</a>
+                  </li>
+                  <li className="footer-link footer-links-help">
+                    <a href="/auth/forgot-password">Forgot Password</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="footer-left">
+            <h3>Ya Kei ta halne ho k halne sochna sakina</h3>
+          </div>
+        </div>
+        <div className="footer-small footer-flex">
+          <div className="footer-small-left footer-flex">
+            <div className="footer-small-img">
+              <img src={Esewa} alt="" />
+            </div>
+            <div className="footer-small-img">
+              <img src={Khalti} alt="" />
+            </div>
+          </div>
+          <div className="footer-small-right footer-flex">
+            <div className="footer-small-icon">
+              <FaDiscord />
+            </div>
+            <div className="footer-small-icon">
+              <FaFacebookF />
+            </div>
+            <div className="footer-small-icon">
+              <FaYoutube />
+            </div>
+            <div className="footer-small-icon">
+              <FaInstagram />
+            </div>
+            <div className="footer-small-icon">
+              <FaXTwitter />
+            </div>
+            <div className="footer-small-icon">
+              <FaTiktok />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="footer-down">
+        <div className="footer-copyright">
+          © 2024 | LynxLine Limited | All rights reserved | Unleash Your Inner
+          Beast
+        </div>
+      </div>
+    </div>
   );
 };
 
